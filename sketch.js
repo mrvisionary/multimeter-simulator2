ced1bfc009c1463aaef357f147a7d1afc377f80f
mrvisionary <mrmarcus29@gmail.com>
2025-07-27 14:58:02 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let voltage = 9; // Default simulated voltage
 let dialAngle = 0;
+let currentMode = "OFF";
 
 function setup() {
   let canvas = createCanvas(400, 400);
@@ -7,7 +7,8 @@ function setup() {
 }
 
 function draw() {
-  background('#e0f7fa'); // light teal background
+  background('#e0f7fa'); // light teal
+  updateMode();
   drawMeter();
   drawProbes();
   drawLabel();
@@ -17,7 +18,7 @@ function drawLabel() {
   textSize(18);
   fill(50);
   textAlign(CENTER);
-  text("AutoTrainer DMM", width / 2, 90); // brand name above screen
+  text("AutoTrainer DMM", width / 2, 90);
 }
 
 function drawMeter() {
@@ -25,17 +26,40 @@ function drawMeter() {
   stroke(0);
   rect(100, 100, 200, 300, 20); // meter body
 
+  // Display reading based on currentMode
   fill(0);
   textSize(24);
   textAlign(CENTER, CENTER);
-  text(`${voltage.toFixed(2)} V`, 200, 130); // voltage reading
+
+  let displayValue = "";
+  switch (currentMode) {
+    case "OFF":
+      displayValue = "--.--";
+      break;
+    case "DCV":
+      displayValue = "12.64 V";
+      break;
+    case "ACV":
+      displayValue = "119.0 V~";
+      break;
+    case "Ohms":
+      displayValue = "3.2 Ω";
+      break;
+    case "Beep":
+      displayValue = "🔊";
+      break;
+    case "Amps":
+      displayValue = "0.45 A";
+      break;
+  }
+  text(displayValue, 200, 130);
 
   // Dial face
   push();
   translate(200, 220);
   rotate(dialAngle);
   strokeWeight(4);
-  line(0, 0, 0, -40); // dial needle
+  line(0, 0, 0, -40); // needle
   pop();
 
   strokeWeight(1);
@@ -57,5 +81,24 @@ function keyPressed() {
     dialAngle -= PI / 12;
   } else if (keyCode === RIGHT_ARROW) {
     dialAngle += PI / 12;
+  }
+}
+
+function updateMode() {
+  let angle = dialAngle % TWO_PI;
+  if (angle < 0) angle += TWO_PI;
+
+  if (angle < PI / 6) {
+    currentMode = "OFF";
+  } else if (angle < PI / 3) {
+    currentMode = "DCV";
+  } else if (angle < PI / 2) {
+    currentMode = "ACV";
+  } else if (angle < 2 * PI / 3) {
+    currentMode = "Ohms";
+  } else if (angle < 5 * PI / 6) {
+    currentMode = "Beep";
+  } else {
+    currentMode = "Amps";
   }
 }
